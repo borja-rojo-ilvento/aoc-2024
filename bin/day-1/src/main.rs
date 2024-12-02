ec5936159a5488edf42aeb4dc343f2ac6443585d
Borja Rojo Ilvento <borja.rojo@gmail.com>
2024-12-02 16:15:15 -0500

    Correct day-1.

--- bin/day-1/src/main.rs
+++ bin/day-1/src/main.rs
@@ -1,24 +1,21 @@
-use std::fs;
 use std::iter::zip;
 use itertools::Itertools;
 
 fn main() {
     let contents = include_str!("list.txt");
+    // println!("{:?}", contents);
+    // let lines = contents.lines();
+    // println!("{:?}", lines);    
     let (mut left, mut right): (Vec<i32>, Vec<i32>) = contents
-        .lines()
-        .flat_map(|line| line.split("\t"))
+        .split_whitespace()
         .filter_map(|s| s.parse::<i32>().ok())
         .tuples()
         .unzip();
-    left = left.sort();
-    right = right.sort();
+    left.sort();
+    right.sort();
 
-    let result: u32 = zip(left.clone(), right.clone())
-        .map(|(a, b)| a.abs_diff(b))
+    let result: u32 = zip(left, right)
+        .map(|(left, right)| left.abs_diff(right))
         .sum();
-
-
-    println!("{:?}", left);
-    println!("{:?}", right);
     println!("{}", result);
 }
