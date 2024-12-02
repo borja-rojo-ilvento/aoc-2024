c28d3067278ccf325f75259bd3d9a68e269e0f9c
Borja Rojo Ilvento <borja.rojo@gmail.com>
2024-12-01 23:11:51 -0500

    Add itertools to Cargo.toml. Finish skeleton implementation of algorithm.

--- bin/day-1/src/main.rs
+++ bin/day-1/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs;
+use std::iter::zip;
+use itertools::Itertools;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("list.txt").expect("Failed to read file");
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = contents
+        .lines()
+        .flat_map(|line| line.split("\t"))
+        .filter_map(|s| s.parse::<i32>().ok())
+        .tuples()
+        .unzip();
+    left.sort();
+    right.sort();
+
+    let result: u32 = zip(left, right)
+        .map(|(a, b)| a.abs_diff(b))
+        .sum();
+
+
+    
+    
+    println!("{}", result);
 }
