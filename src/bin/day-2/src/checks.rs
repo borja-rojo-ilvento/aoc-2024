ca03b5bc737a6b178260b5f25b071888e969c38d
Borja Rojo Ilvento <borja.rojo@gmail.com>
2024-12-02 21:28:22 -0500

    Move main.rs files into src/bin

--- src/bin/day-2/src/checks.rs
+++
@@ -1,8 +0,0 @@
-fn too_different(left: i32, right: i32) -> bool {
-    let diff: u32 = left.abs_diff(right);
-    diff >= 1 && diff <= 3
-}
-
-fn changes_direction(left: i32, middle: i32, right: i32) -> bool {
-    (left < middle && middle > right) || (left > middle && middle < right)
-}
